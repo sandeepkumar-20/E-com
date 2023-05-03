059ed9db5f846f5177de50303f4f2c9b7ac81d29
Sandeep Kumar <75008149+sandeepkumar-20@users.noreply.github.com>
2023-05-04 03:14:27 +0530

    Add files via upload

---
+++ mern-ecommerce/client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello from Gurpreet</div>;
+
+export default App;
---
+++ mern-ecommerce/client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '0 to 1000',
+    array: [0, 1000],
+  },
+  {
+    _id: 2,
+    name: '1000 to 2500',
+    array: [1000, 2500],
+  },
+  {
+    _id: 3,
+    name: '2500 to 5000',
+    array: [2500, 5000],
+  },
+  {
+    _id: 4,
+    name: '5000 to 7000',
+    array: [5000, 7000],
+  },
+  {
+    _id: 5,
+    name: 'More than 5000',
+    array: [5000, 100000],
+  },
+];
